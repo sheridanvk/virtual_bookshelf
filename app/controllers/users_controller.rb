5ebde34923fbf9f6fc259ff940d311eb3aa2336d
sheridanvk <sheridan.kates@gmail.com>
2016-06-14 22:34:09 -0400

    Fixed creating new accounts in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,27 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :add_goodreads]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   # GET /users/1.json
   def show
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
 
   # GET /users/1/edit
   def edit
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    # @user = User.new(user_params)
-    # respond_to do |format|
-    #   if @user.save
-    #     log_in @user
-    #     format.html { redirect_to get_oauth_url, id: @user.id}
-    #     # format.json { render :show, status: :created, location: @user }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @user == current_user
+        format.html { render :edit }
+      else
+        format.html { redirect_to @user, notice: 'You are not permitted to edit this user\'s account.' }
+      end
+    end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    # TODO make sure only the logged in user can update the user.
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -48,16 +30,6 @@ class UsersController < ApplicationController
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
+
   # Remove model name from the URL
   resources :users, path: ''
   get '/:friendly_id', to: 'users#show'
@@ -20,8 +21,6 @@ Rails.application.routes.draw do
       get 'check_goodreads_authentication'
     end
   end
-
-  resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
